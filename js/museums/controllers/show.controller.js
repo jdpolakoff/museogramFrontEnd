eaa5b36d6f4638aabecf276b7282f248c359ab76
Liz Jewell <eghjewell@gmail.com>
2017-08-22 19:01:22 -0400

    some styling and view fixing

--- js/museums/controllers/show.controller.js
+++ js/museums/controllers/show.controller.js
@@ -9,4 +9,11 @@ angular
 
 function MuseumShowControllerFunction ($stateParams, Museum, Artwork) {
   this.museum = Museum.get({id: $stateParams.museum_id})
+  let count = 100
+  let arts = []
+  for (i=1; i < count; i++) {
+    art = Artwork.get({id: i})
+    arts.push(art)
+  }
+  this.someArt = arts
 }
--- js/reviews/controllers/new.controller.js
+++ js/reviews/controllers/new.controller.js
@@ -2,17 +2,22 @@ angular
 .module('museoGram')
 .controller('ReviewsNewController', [
   '$stateParams',
+  '$state',
   'Museum',
   'Artwork',
   'Review',
   ReviewsNewControllerFunction
 ])
 
-function ReviewsNewControllerFunction ($stateParams, Museum, Artwork, Review) {
+function ReviewsNewControllerFunction ($stateParams, $state, Museum, Artwork, Review) {
   // this.artwork = Artwork.get({id: $stateParams.artwork_id})
   this.newReview = new Review()
-  this.newReview.artwork_id = $stateParams.artwork_id 
+  this.newReview.artwork_id = $stateParams.artwork_id
   this.create = function () {
-    this.newReview.$save()
+    this.newReview.$save(() => {
+      $state.go('reviewIndex')
+    }
+
+    )
   }
 }
