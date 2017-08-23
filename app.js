a61f7eeec6a78de7de6b8751505d7cb8bf3e89e2
ramiGA <ramisn3@gmail.com>
2017-08-23 10:48:17 -0400

    changes

--- app.js
+++ app.js
@@ -2,9 +2,6 @@ angular
 .module('museoGram', [
   'ui.router',
   'ngResource',
-<<<<<<< HEAD
-  'ui.bootstrap'
-=======
-  'wu.masonry',
->>>>>>> 8564f3c26e26b9ba13bf1c55798d0ce2e8ee2c86
+  'ui.bootstrap',
+  'wu.masonry'
 ])
