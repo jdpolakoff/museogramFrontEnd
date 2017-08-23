f885e763e3991dad2e01120d789e14d4b406f806
Jonathan Polakoff <jdpolakoff@gmail.com>
2017-08-23 07:29:31 -0400

    set min height on imgs

--- js/museums/controllers/show.controller.js
+++ js/museums/controllers/show.controller.js
@@ -9,7 +9,7 @@ angular
 
 function MuseumShowControllerFunction ($stateParams, Museum, Artwork) {
   this.museum = Museum.get({id: $stateParams.museum_id})
-  let count = 100
+  let count = 25
   let arts = []
   for (i=1; i < count; i++) {
     art = Artwork.get({id: i})
