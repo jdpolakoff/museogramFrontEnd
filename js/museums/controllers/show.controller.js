da72aff97441f877f3ee6108469621fdcbbf3665
Jonathan Polakoff <jdpolakoff@gmail.com>
2017-08-23 15:08:18 -0400

    mus controller logic

--- js/museums/controllers/show.controller.js
+++ js/museums/controllers/show.controller.js
@@ -9,34 +9,26 @@ angular
 
 function MuseumShowControllerFunction ($stateParams, Museum, Artwork) {
   this.museum = Museum.get({id: $stateParams.museum_id})
-  this.artworks = this.museum.artworks
-  let count = 25
-  let arts = []
-  for (i=1; i < count; i++) {
-    art = Artwork.get({id: i})
-    arts.push(art)
-  }
-  this.someArt = arts
 
-  let count_draw = 650
-  let drawings = []
-  for (i=600; i < count_draw; i++) {
-    art = Artwork.get({id: i})
-    drawings.push(art)
-  }
-  this.drawings = drawings
+  this.artworks = Artwork.query()
 
-  // this.sculptures = Artwork.findAllByAttribute('category', 'Sculptures')
-  function filterFunction (query) {
-    console.log(query);
-  }
-
+  // let arts = []
+  // for (i=1; i < count; i++) {
+  //   art = Artwork.get({id: i})
+  //   arts.push(art)
+  // }
+  // this.someArt = arts
+  //
+  this.category = 
 }
 
 
 
-// if (arts.length === count) {
-//   setTimeout(function() {
-//   location.reload()
-// }, 1000)
-// }
+
+
+
+if (arts.length === count) {
+  setTimeout(function() {
+  location.reload()
+}, 1000)
+}
