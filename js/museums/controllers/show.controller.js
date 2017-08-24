4a03c297adc6467214c252db60322b84619a9205
Jonathan Polakoff <jdpolakoff@gmail.com>
2017-08-24 12:39:20 -0400

    styles

--- js/museums/controllers/show.controller.js
+++ js/museums/controllers/show.controller.js
@@ -8,12 +8,25 @@ angular
 ])
 
 function MuseumShowControllerFunction ($stateParams, Museum, Artwork) {
+  this.loaded = false
+  this.loading = true
+
   this.museum = Museum.get({id: $stateParams.museum_id}, (museum) => {
     this.artworks = museum.artworks
+    this.loading = false
+    this.loaded = true
   })
   this.category = ''
   this.setCategory = setCategory
   function setCategory(input) {
     this.category = input
+  }
+  this.limit = 25
+  this.increment = increment
+  function increment (increment) {
+    this.limit += increment
+    if (this.limit > 75) {
+      this.loaded = false
+    }
   }
 }
